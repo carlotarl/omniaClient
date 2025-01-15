b5a74897131aee383f5b06c5188ac76013940cee
carlotarl <84837470+carlotarl@users.noreply.github.com>
2025-01-15 11:36:47 +0100

    Add files via upload

---
+++ PanelControl.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import { FaRegPlusSquare } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+import { FaTrashAlt } from "react-icons/fa";
+import { MdModeEditOutline } from "react-icons/md";
+
+
+
+function PanelDeControl() {
+
+    const [servicios, setServicios] = useState([]);
+
+    useEffect(() => {
+
+        fetch('/servicios').then(res => {console.log(res);return res.json()})
+
+            .then(datos => {
+                setServicios(datos)
+            })
+
+    }, []);
+
+
+
+    return (
+
+        <section className='serviciosCrud'>
+
+            {servicios.map(servicio => {
+
+                return <div key={servicio.id}>
+
+                    <h2>{servicio.nombre}</h2>
+                    <p>{servicio.descripcion}</p>
+                    <p>{servicio.duracion}</p>
+                    <p>{servicio.precio}</p>
+
+
+                    <div className='botones'>
+                        <Link to={'/borrarServicio/' + servicio.id}><FaTrashAlt /></Link>
+                        <Link to={'/EditarServicio/' + servicio.id}><MdModeEditOutline/></Link>  
+                    </div>
+
+
+
+                </div>
+
+
+
+            })}
+
+            <div className='nueva'>Añadir nueva clase: 
+                <div className='botones'>
+                    <Link to={'/nuevoServicio'}><FaRegPlusSquare /></Link>
+                </div>
+            </div>
+
+
+
+        </section>
+
+    )
+
+}
+
+
+
+export default PanelDeControl 
