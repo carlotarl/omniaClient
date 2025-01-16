b5dfbb25be3f74838018d9a9c3faaf4c39f5a61c
carlotarl <84837470+carlotarl@users.noreply.github.com>
2025-01-16 09:18:47 +0100

    Add files via upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,23 +3,17 @@ import {FaWhatsapp,FaInstagram } from "react-icons/fa";
 import { RiFacebookCircleLine } from "react-icons/ri";
 import { PiTiktokLogo } from "react-icons/pi";
 
-
-
 function Footer() {
   return (
-
     <footer className='footer'>
         <div className='footer__rrss'>
         <PiTiktokLogo  className='footer__red'/>
         <RiFacebookCircleLine className='footer__red'/>
         <FaWhatsapp className='footer__red'/>
         <FaInstagram className='footer__red'/>
-
         </div>
-
         <p>&copy; Todos los derechos reservados</p>
     </footer>
-
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useUser } from '../context/UsuarioContext'
 
 function Header() {
 
-  const {usuario} = useUser();
-
   return (
     <header className='header'>
-
         <img src="logo.png" alt="Logo" className='header__imagen'/>
-
         <nav className='header__nav'>
-
             <ul className='header__lista'>
-
                 <li className="header__elementoLista"><Link to={'/'} className='header__link'>Inicio</Link></li>
                 <li className="header__elementoLista"><Link to={'/quienes'} className='header__link'>Quienes Somos</Link></li>
                 <li className="header__elementoLista"><Link to={'/servicios'} className='header__link'>Servicios</Link></li>
                 <li className="header__elementoLista"><Link to={'/panel'} className='header__link'>Panel de control</Link></li>
-
             </ul>
-
         </nav>
-
     </header>
   )
 }
